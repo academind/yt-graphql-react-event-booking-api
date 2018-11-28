cc7e5e9386b40629565e5691819584e95a542c0a
Maximilian Schwarzmüller <company@academind.com>
2018-11-28 16:10:31 +0100

    finished starting setup

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
