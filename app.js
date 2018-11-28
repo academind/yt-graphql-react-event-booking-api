9e8b2070119e3e8e4dd8c6aec2558ee735095826
Maximilian Schwarzmüller <company@academind.com>
2018-11-28 15:41:48 +0100

    basic setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000);
