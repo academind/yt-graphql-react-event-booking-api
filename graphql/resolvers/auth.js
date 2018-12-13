47bd1aaaaca4bfe421978d46bd983895856c54a6
Maximilian Schwarzmüller <company@academind.com>
2018-12-13 10:31:30 +0100

    added login

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/user');
 
@@ -22,5 +23,23 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error('User does not exist!');
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error('Password is incorrect!');
+    }
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      'somesupersecretkey',
+      {
+        expiresIn: '1h'
+      }
+    );
+    return { userId: user.id, token: token, tokenExpiration: 1 };
   }
 };
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -25,6 +25,12 @@ type User {
   createdEvents: [Event!]
 }
 
+type AuthData {
+  userId: ID!
+  token: String!
+  tokenExpiration: Int!
+}
+
 input EventInput {
   title: String!
   description: String!
@@ -40,6 +46,7 @@ input UserInput {
 type RootQuery {
     events: [Event!]!
     bookings: [Booking!]!
+    login(email: String!, password: String!): AuthData!
 }
 
 type RootMutation {
