90a74044d6aed4ecccc7c506f9859d3cb46f8b2a
Maximilian Schwarzmüller <company@academind.com>
2019-01-02 15:52:29 +0100

    added navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
@@ -11,12 +12,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './MainNavigation.css';
+
+const mainNavigation = props => (
+  <header className="main-navigation">
+    <div className="main-navigation__logo">
+      <h1>EasyEvent</h1>
+    </div>
+    <nav className="main-navigation__items">
+      <ul>
+        <li>
+          <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+        <li>
+          <NavLink to="/events">Events</NavLink>
+        </li>
+        <li>
+          <NavLink to="/bookings">Bookings</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default mainNavigation;
