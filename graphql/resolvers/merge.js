95d4e15fe95c3b8940e9a5be0edc330f2a0c4feb
Maximilian Schwarzmüller <company@academind.com>
2019-02-12 14:31:48 +0100

    fixed bug

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -4,17 +4,22 @@ const Event = require('../../models/event');
 const User = require('../../models/user');
 const { dateToString } = require('../../helpers/date');
 
-const eventLoader = new DataLoader((eventIds) => {
+const eventLoader = new DataLoader(eventIds => {
   return events(eventIds);
 });
 
 const userLoader = new DataLoader(userIds => {
-  return User.find({_id: {$in: userIds}});
+  return User.find({ _id: { $in: userIds } });
 });
 
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
+    events.sort((a, b) => {
+      return (
+        eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+      );
+    });
     return events.map(event => {
       return transformEvent(event);
     });
